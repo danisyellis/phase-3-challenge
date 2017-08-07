a38f39388a3b5dfcfe7318a9501445882adff31d
Danielle Gellis <danisyellis@gmail.com>
2017-08-07 16:58:42 -0700

    part 1 POST route is complete- body-parser was added

--- part-1/app.js
+++ part-1/app.js
@@ -3,9 +3,14 @@
 const express = require('express');
 const app = express();
 
-const logger = require("morgan");
-app.use(logger("dev"));
+//middleware
+const logger = require('morgan');
+app.use(logger('dev'));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
+//routes
 app.get("/api/days/:day", ((req, res) => {
   const daysOfWeek = {monday: 1, tuesday:2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7};
   const day = req.params.day.toLowerCase();
@@ -19,6 +24,17 @@ app.get("/api/days/:day", ((req, res) => {
     }
 }));
 
-//app.post()
+app.post("/api/array/concat", (req, res) => {
+  res.set("Content-Type", "application/json");
+  const array1 = req.body.array1;
+  const array2 = req.body.array2;
+  if(Array.isArray(array1) && Array.isArray(array2)) {
+    const arrayToSend = array1.concat(array2);
+    res.send({"result": arrayToSend});
+  } else {
+    res.status(400);
+    res.send({"error": "Input data should be of type Array."});
+  }
+});
 
 app.listen(3000, console.log("Hi there! I'm ready!"));
