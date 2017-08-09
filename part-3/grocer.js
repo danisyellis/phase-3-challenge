1720bc4bb4b40160cb4d701e57506c7f8d5bd919
Danielle Gellis <danisyellis@gmail.com>
2017-08-09 03:48:20 -0700

    can navigate via sidebar, can change number in cart

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log("In the JS :-)");
+//Clicking on "Add to cart" will update the number displayed next to the "Cart" button to show the total number of items in the user's cart
+const numberInCartEl = document.getElementById("cart-item-count");
+const cartButtonEl = document.getElementById("cart-button");
+let numberInCart = 0;
+numberInCartEl.innerHTML = `(${numberInCart})`;
+
+cartButtonEl.addEventListener("click", () => {
+  numberInCart += 1;
+  numberInCartEl.innerHTML = `(${numberInCart})`;
+});
