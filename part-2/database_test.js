ccf19afbcb334a6fc0f4dadc93abb276246d4558
Danielle Gellis <danisyellis@gmail.com>
2017-08-09 00:49:32 -0700

    all tests for product-list and shopper-orders working

--- part-2/database_test.js
+++ part-2/database_test.js
@@ -1,12 +1,36 @@
 'use strict';
 
 const mocha = require('mocha');
-//const chai = require('chai').expect;
-const { expect } = require('chai');
+const expect = require('chai').expect;
+//const { expect } = require('chai');
 const dbFuncs = require('./database.js');
 
 describe("testing product-list", function() {
-  it("returns all products in a given section, along with the section name", function() {
-    expect(dbFuncs.getProducts("frozen")).to.equal(5);
+  it("returns items from the given section", function() {
+    return dbFuncs.getProducts("frozen")
+    .then(data => {expect(data[0].name).to.equal('Ice Cream');});
+  });
+  it("also returns the section name of the given section", function() {
+    return dbFuncs.getProducts("frozen")
+    .then(data => {expect(data[0].section).to.equal('frozen');});
+  });
+  it("returns all items in that section", function() {
+    return dbFuncs.getProducts("frozen")
+    .then(data => {expect(data.length).to.equal(3);});
+  });
+});
+
+describe("testing shopper-orders", function() {
+  it("returns the ids of orders for the given shopper", function() {
+    return dbFuncs.getOrdersForShopper(1)
+    .then(data => {expect(data[1].order_id).to.equal(3);});
+  });
+  it("returns all orders for the given shopper", function() {
+    return dbFuncs.getOrdersForShopper(1)
+    .then(data => {expect(data.length).to.equal(3);});
+  });
+  it("also returns the summed price of all items in each order", function() {
+    return dbFuncs.getOrdersForShopper(1)
+    .then(data => {expect(data[0].sum).to.equal('44.34');});
   });
 });
